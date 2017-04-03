2b827dff5431355faf256979198cc67d73880afe
Natalia <tester@wp.pl>
2017-04-03 21:07:43 +0200

    zmiana imienia i wiadomości

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,8 +3,8 @@ from formater import get_formatted
 from formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "Natalia"
-msg = "Hello!"
+moje_imie = "HolaAmigo"
+msg = "Que tall, es muy bueno!"
 
 @app.route('/')
 def index():
