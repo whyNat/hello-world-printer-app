52486ee901c3c76ae47569fe7fdc7e4a138b8448
whyNat <nwaszak@wp.pl>
2017-04-22 10:50:45 +0200

    klik w link

--- hello_3.py
+++ hello_3.py
@@ -11,19 +11,18 @@ class SearchText(unittest.TestCase):
         driver = self.driver
         # driver.implicitly_wait(30)
         driver.maximize_window()
-        # navigate to the application home page
-        driver.get("http://www.google.com/")
+
 
     def test_search_by_text(self):
         driver = self.driver
+        driver.get("http://www.wsb.pl")
         # get the search textbox
-        search_field = driver.find_element_by_id("lst-ib")
-        search_field.clear()
+        #search_field = driver.find_element_by_css_selector("#block-menu-menu-wybierz-studia-w-wsb > div > ul > li:nth-child(2) > a")
         # enter search keyword and submit
-        search_field.send_keys("Selenium")
+        link = driver.find_element_by_link_text("Studia podyplomowe")
+        link.click()
         sleep(5)
-        search_field.submit()
-        sleep(5)
+
 
     def tearDown(self):
         # close the browser window
