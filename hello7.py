fd67c5dcad43eb75ade3858aa4d8239125945482
whyNat <nwaszak@wp.pl>
2017-04-22 15:03:25 +0200

    wybór pola

--- hello7.py
+++ hello7.py
@@ -5,11 +5,22 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
+import my_credentals as mc
+from time import sleep
 
-
+valid_name = "Dick"
+valid_surname = "Laurent"
+telefon = "123123123"
+invalid_email = "hhjkj.pl"
+valid_password = "Qshiukk12"
 
 """
-1. Wejdz na https://wizzair.com/pl-pl/main-page#/
+Rejestracja nowego użytkownika na stronie wizzair.com
+
+Przypadki testowe:
+I Rejestracja nowego użytkownika używając adresu email - dane niepoprawne (niekompletny email)
+Kroki:
+"1. Wejdz na https://wizzair.com/pl-pl/main-page#/
 2. Kliknij w prawym górnym rogu ZALOGUJ SIĘ
 3. Wybierz rejestracja
 4. Wprowadź imię
@@ -22,32 +33,62 @@ from time import sleep
 11. Akceptuj politykę prywatności
 12. Kliknij przycisk ZAREJESTRUJ SIĘ"
 
+Oczekiwany rezultat:
+Przycisk "zarejestruj się" jest nieaktywny
+Uzytkownik dostaje informację, że wprowadzony e-mail jest niepoprawny
+
 """
-
-valid_name = "Dick"
-valid_surname = "Laurent"
-telefon = "123123123"
-invalid_email = "hhjkj.pl"
-valid_password = "Qshiukk12"
-
 
 class WizzairRegistration(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Chrome()
+        #self.driver.implicitly_wait(10)
         self.driver.maximize_window()
 
     def test_registration_wrong_email(self):
         driver = self.driver
-        driver.get("https://wizzair.com/#/")
-        zaloguj_btn = driver.find_element_by_css_selector("li.navigation__item:nth-child(3) > button:nth-child(1)").click()
-        rejestracja_btn = driver.find_element_by_css_selector("p.login-form__footer:nth-child(6) > button:nth-child(1)").click
-        imie_field = driver.find_element_by_xpath("//input[@placeholder='First name']")
+        driver.get("https://wizzair.com/pl-pl/main-page#/")
+        zaloguj_btn = driver.find_element_by_css_selector("#app > header > div > nav > ul > li:nth-child(3) > button")
+        zaloguj_btn.click()
+        rejestracja_btn = driver.find_element_by_xpath('//*[@id="login-modal"]/form/div/p/button')
+        rejestracja_btn.click()
+        #imie_field =  driver.find_element_by_css_selector("#registration-modal > \
+        #form > div.modal-popup__content.register-form__content > div.register-form__name-field > \
+        #div.input-set__container.register-form__name-field__input.register-form__name-field__input--first > input")
+        imie_field = driver.find_element_by_xpath("//input[@placeholder='Imię']")
+        imie_field.send_keys(valid_name)
+        nazwisko_field = driver.find_element_by_xpath("//input[@placeholder='Nazwisko']")
+        nazwisko_field.send_keys(valid_surname)
+        gender_switch = driver.find_element_by_id("register-gender-male")
+        driver.execute_script("arguments[0].click()", gender_switch)
+        #gender_switch = driver.find_element_by_css_selector('div[class="switch switch--gender switch--full-width gutter-bottom"] label[for=register-gender-male]')
+        #gender_switch.click()
+        telefon_field = driver.find_element_by_css_selector("input[type=tel]")
+        telefon_field.send_keys(telefon)
+        email_field = driver.find_element_by_css_selector("input[data-test=booking-register-email]")
+        email_field.send_keys(invalid_email)
+        password_field = driver.find_element_by_css_selector("input[data-test=booking-register-password]")
+        password_field.send_keys(valid_password)
+        country_field = driver.find_element_by_css_selector("input[data-test=booking-register-country]")
+        country_field.click()
+        #country_to_choose = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located(
+        #    (By.XPATH, '//*[@class="register-form__country-container__locations"]/label[164]')))
+        country_to_choose = driver.find_element_by_xpath('//*[@class="register-form__country-container__locations"]/label[164]')
+        country_to_choose.location_once_scrolled_into_view
+        #WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located(
+        #   (By.XPATH, '//*[@class="register-form__country-container__locations"]/label[164]')))
+        country_to_choose.click()
+        privacy_policy = driver.find_element_by_xpath('//*[@data-test="booking-register-privacy-policy"]')
+        privacy_policy.click()
+        zarejestruj_btn = driver.find_element_by_css_selector(".button[type=submit][data-test=booking-register-submit]")
+        self.assertFalse(zarejestruj_btn.is_enabled(), "Szlag rafił!")
+        error_notice = driver.find_element_by_css_selector("div[class='error-notice error-notice--compact'] span")
+        self.assertEqual(unicode(error_notice.text), u"Nieprawidłowy adres e-mail")
+
 
     def tearDown(self):
-        # close the browser window
-        self.driver.close()
+        self.driver.quit()
 
-
-if __name__ == '__main__':
-    unittest.main(verbosity=2)
+if __name__ == "__main__":
+    unittest.main()
