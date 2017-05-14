072c6d74837ea46d700bd51796eaf5213479c37b
whyNat <nwaszak@wp.pl>
2017-05-14 12:27:02 +0200

    zmiana imienia

--- test/test_views.py
+++ test/test_views.py
@@ -14,4 +14,4 @@ class FlaskrTestCase(unittest.TestCase):
 
     def test_msg_with_output(self):
         rv = self.app.get('/?output=json')
-        self.assertEquals('{ "imie":"HolaAmigo", "mgs":Que tall, es muy bueno!"}', rv.data)
+        self.assertEquals('{ "imie":"John", "mgs":hi there"}', rv.data)
